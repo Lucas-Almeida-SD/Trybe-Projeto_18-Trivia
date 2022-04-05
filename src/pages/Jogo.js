fda933ae629eec777a010016e7bfef5ec030fa91
Cinthya Morales <cinthya.morales@outlook.com>
2022-04-05 18:54:57 -0300

    Requisito 3 CERTO

--- src/pages/Jogo.js
+++ src/pages/Jogo.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 class Jogo extends React.Component {
   render() {
     return (
       <div>
-        <Link to="/configuracao">
-          <button type="button" data-testid="btn-settings">Configurações</button>
-        </Link>
+        <h1>Pagina do jogo!!!</h1>
       </div>
     );
   }
