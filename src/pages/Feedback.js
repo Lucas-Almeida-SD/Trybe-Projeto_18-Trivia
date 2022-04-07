4ca876c33a8fe1d08094fdb80588d3d92647f56e
Robert-as <robertalvessilv@hotmail.com>
2022-04-07 19:07:35 -0300

    Requisito 14 ok

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -5,11 +5,21 @@ import Header from '../components/Header';
 
 class Feedback extends React.Component {
   render() {
-    const { assertions } = this.props;
+    const { assertions, score } = this.props;
+    localStorage.setItem('score', score);
+    localStorage.setItem('assertions', assertions);
     const min = 3;
     return (
       <>
         <Header />
+        <p>
+          Pontuação:
+          <span data-testid="feedback-total-score">{ score }</span>
+        </p>
+        <p>
+          Total de acertos:
+          <span data-testid="feedback-total-question">{ assertions }</span>
+        </p>
         {assertions < min
           ? <p data-testid="feedback-text">Could be better...</p>
           : <p data-testid="feedback-text">Well Done!</p>}
@@ -19,6 +29,7 @@ class Feedback extends React.Component {
 }
 const mapStateToProps = (state) => ({
   assertions: state.player.assertions,
+  score: state.player.score,
 });
 
 export default connect(mapStateToProps)(Feedback);
