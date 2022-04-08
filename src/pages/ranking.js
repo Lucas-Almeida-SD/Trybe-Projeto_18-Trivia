60255a0bc413e6ce7d360560a7befb45390d7b78
Cinthya Morales <cinthya.morales@outlook.com>
2022-04-08 15:58:26 -0300

    Requisito 17

--- src/pages/ranking.js
+++ src/pages/ranking.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Ranking extends React.Component {
   render() {
     return (
       <div>
         <h1 data-testid="ranking-title">Pagina de Ranking</h1>
+        <Link to="/">
+          <button type="button" data-testid="btn-go-home">Home</button>
+        </Link>
       </div>
     );
   }
